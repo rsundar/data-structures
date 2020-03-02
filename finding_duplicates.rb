0b047ebe5821381db32ed7051c0a78740dc7ee19
rsundar <rohan.s.1987@gmail.com>
2020-03-02 23:06:38 +0530

    finding duplicates draft

---
+++ finding_duplicates.rb
@@ -0,0 +1,18 @@
+require 'set'
+
+def find_duplicates(array)
+  # write your code here
+  s = Set.new
+  array.each_with_index do |i,j|
+    s.add(j) if array.count(j) > 1
+  end
+  s.to_a
+end
+
+p find_duplicates([1, 2, 3, 1, 5, 6, 7, 8, 5, 2])
+# => [1, 5, 2]
+
+p find_duplicates([3, 501, 17, 23, -43, 67, 5, 888, -402, 235, 77, 99, 311, 1, -43])
+# => [-43]
+
+p find_duplicates([4376, -345, -345, 4376, -345, 84945, 4376, -345, -26509])
